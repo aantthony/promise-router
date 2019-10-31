2b53870d09400a00098c8b6aecb6778e3421fee1
Anthony Foster <277425+aantthony@users.noreply.github.com>
2019-10-31 17:12:28 +1100

    Add more types

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,32 @@
 import * as express from 'express';
 
-type WithBody<C, BodyType=unknown> = C & { body: BodyType }
+export type JsonValue = string | number | boolean | JsonObject | null;
+interface JsonObject {
+  [x: string]: JsonValue | undefined;
+}
+interface JsonArray extends Array<JsonValue> { }
+
+export type JsonSerializableValue = string | number | boolean | Date | JsonSerializableObject | JsonSerializableArray | null;
+export interface JsonSerializableObject {
+  [x: string]: JsonSerializableValue | undefined;
+}
+interface JsonSerializableArray extends Array<JsonSerializableValue> { }
+
+type WithBody<C, BodyType> = C & { body: BodyType };
+
+export type ResponseBody = JsonSerializableValue;
+
+export type Request<C> = Omit<express.Request, 'body'> & C;
 
 interface Handler<C,R=any> {
-  (req: C & express.Request, res: express.Response): Promise<R> | R | void;
+  (req: Request<C>, res: express.Response): Promise<R|void> | R | void;
 }
 
 interface CanHandle<C> extends express.RequestHandler {
   (req: C & express.Request, res: express.Response): void;
 }
 
-export interface Router<C extends {},R=any> extends CanHandle<C> {
+export interface Router<C extends {}, R=JsonSerializableValue, BodyType=JsonValue> extends CanHandle<C> {
   use(fn: Handler<C,void>): this;
 
   // Mount another router
@@ -23,9 +39,9 @@ export interface Router<C extends {},R=any> extends CanHandle<C> {
 
   param(name: string, handler: Handler<C,R>): this;
   get(path: string, handler: Handler<C,R>): this;
-  put<T = {[key: string]: unknown}>(path: string, handler: Handler<WithBody<C, T>,R>): this;
-  post<T = {[key: string]: unknown}>(path: string, handler: Handler<WithBody<C, T>,R>): this;
-  delete<T = {[key: string]: unknown}>(path: string, handler: Handler<WithBody<C, T>,R>): this;
+  put<T = BodyType>(path: string, handler: Handler<WithBody<C, T>,R>): this;
+  post<T = BodyType>(path: string, handler: Handler<WithBody<C, T>,R>): this;
+  delete<T = BodyType>(path: string, handler: Handler<WithBody<C, T>,R>): this;
 }
 
-export default function createRouter<C,T=any>(): Router<C,T>;
+export default function createRouter<C,R=any,BodyType=JsonObject>(): Router<C,R,BodyType>;
